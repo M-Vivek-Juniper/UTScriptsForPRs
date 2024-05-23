3c55438558a29694013015017868ff72f3c485d7
M-Vivek-Juniper <vm@juniper.net>
2024-05-23 11:53:26 +0530

    Shell script for testing PR 1807557

---
+++ 1807557.sh
@@ -0,0 +1,4 @@
+for  (( ; ; ))
+do
+gnmic -a geneva:50051 -u regress -p MaRtInI --encoding PROTO --sample-interval 5s --stream-mode SAMPLE sub --path /interfaces/interface/subinterfaces/subinterface/  --insecure
+done
